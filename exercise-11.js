97c18b535b198d7e7b19e137768585ddb878489e
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-02-03 21:13:38 +0700

    exer 11

---
+++ exercise-11.js
@@ -0,0 +1,11 @@
+function balikKata(kata) {
+
+    var word = '';
+    
+    for(var i = kata.length-1; i >= 0; i--) {
+        word += kata[i];
+    } 
+    return word;
+  }
+
+  console.log(balikKata('Nama saya Fahmi'))
